276fa8966505e496c62dcc11675b6c4169c63cc8
AlexTheMaggot <alexthemaggot23@gmail.com>
2020-05-27 01:57:42 +0500

    add elements on main-window

--- agado/mainapp/static/js/main.js
+++ agado/mainapp/static/js/main.js
@@ -1,5 +1,29 @@
 $(document).ready(function () {
     setTimeout(function () {
         $('body').css('opacity', '1');
-    }, 500)
+        $('#ticker2').css('transform', 'translateX(200px)');
+
+    }, 500);
+
+    function ticker() {
+        setTimeout(function () {
+            $('#ticker1').css({'opacity': '0', 'transform': 'translateX(-200px)'});
+            $('#ticker2').css({'opacity': '1', 'transform': 'translateX(0)'});
+            $('#ticker3').css('transform', 'translateX(200px)');
+        }, 3500);
+        setTimeout(function () {
+            $('#ticker2').css({'opacity': '0', 'transform': 'translateX(-200px)'});
+            $('#ticker3').css({'opacity': '1', 'transform': 'translateX(0)'});
+            $('#ticker1').css('transform', 'translateX(200px)');
+        }, 6500);
+        setTimeout(function () {
+            $('#ticker3').css({'opacity': '0', 'transform': 'translateX(-200px)'});
+            $('#ticker1').css({'opacity': '1', 'transform': 'translateX(0)'});
+            $('#ticker2').css('transform', 'translateX(200px)');
+        }, 9500);
+    }
+    ticker();
+    setInterval(function () {
+        ticker();
+    }, 9000)
 });
