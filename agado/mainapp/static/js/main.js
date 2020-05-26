2708bcfc837dcae658df136aa3ebca61c6002f40
AlexTheMaggot <alexthemaggot23@gmail.com>
2020-05-27 02:22:59 +0500

    fix ticker

--- agado/mainapp/static/js/main.js
+++ agado/mainapp/static/js/main.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
     setTimeout(function () {
         $('body').css('opacity', '1');
-        $('#ticker2').css('transform', 'translateX(200px)');
+        $('#ticker2').css('transform', 'translateX(400px)');
 
     }, 500);
 
     function ticker() {
         setTimeout(function () {
-            $('#ticker1').css({'opacity': '0', 'transform': 'translateX(-200px)'});
+            $('#ticker1').css({'opacity': '0', 'transform': 'translateX(-400px)'});
             $('#ticker2').css({'opacity': '1', 'transform': 'translateX(0)'});
-            $('#ticker3').css('transform', 'translateX(200px)');
+            $('#ticker3').css('transform', 'translateX(400px)');
         }, 3500);
         setTimeout(function () {
-            $('#ticker2').css({'opacity': '0', 'transform': 'translateX(-200px)'});
+            $('#ticker2').css({'opacity': '0', 'transform': 'translateX(-400px)'});
             $('#ticker3').css({'opacity': '1', 'transform': 'translateX(0)'});
-            $('#ticker1').css('transform', 'translateX(200px)');
+            $('#ticker1').css('transform', 'translateX(400px)');
         }, 6500);
         setTimeout(function () {
-            $('#ticker3').css({'opacity': '0', 'transform': 'translateX(-200px)'});
+            $('#ticker3').css({'opacity': '0', 'transform': 'translateX(-400px)'});
             $('#ticker1').css({'opacity': '1', 'transform': 'translateX(0)'});
-            $('#ticker2').css('transform', 'translateX(200px)');
+            $('#ticker2').css('transform', 'translateX(400px)');
         }, 9500);
     }
     ticker();
